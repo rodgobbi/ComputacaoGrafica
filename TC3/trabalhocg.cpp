7876e903ae5a6284bb471b11203ccad3ed135ad9
RODOLFO GOBBI DE ANGELI <rodolfodeangeli@gmail.com>
2016-10-06 18:30:41 -0300

    improved svg loading

--- TC3/trabalhocg.cpp
+++ TC3/trabalhocg.cpp
@@ -9,7 +9,11 @@ using namespace std;
 
 string ExtractFilePath(string pConfigFilePath);
 void CreateWindow(string pFilePath, GLsizei& pWindowWidth, GLsizei& pWindowHeight);
-GLsizei gWindowWidth, gWindowHeight; 
+Circle ExtractCircleData(TiXmlElement* pElement);
+Car ExtractCarData(TiXmlElement* pElement);
+Rectangle ExtractRectData(TiXmlElement* pElement);
+
+GLsizei gWindowWidth, gWindowHeight;
 Circle gOuterCircle, gInnerCircle, gEnemyCircle1, gEnemyCircle2, gEnemyCircle3;
 Car gPlayerCar;
 Rectangle gStripeRect;
@@ -45,15 +49,15 @@ int main(int argc, char** argv)
 	catch(exception& e){
 		cerr << e.what() << endl;
 		return 1;
-	} 	
+	}
 	catch(string& msg) {
 		cerr << msg << endl;
 		return 1;
-	}	
+	}
 	catch(const char* msg) {
 		cerr << msg << endl;
 		return 1;
-	}	
+	}
 }
 
 string ExtractFilePath(string pConfigFilePath) {
@@ -69,25 +73,17 @@ string ExtractFilePath(string pConfigFilePath) {
 }
 
 void CreateWindow(string pFilePath, GLsizei& pWindowWidth, GLsizei& pWindowHeight) {
-	GLint lX, lY, lW, lH, lR;
-	string lColor;
 	TiXmlDocument doc(pFilePath);
 	if (!doc.LoadFile()) {
 		throw string("Erro ao tentar abrir o arquivo svg.");
 	}
 	TiXmlNode* lNode = doc.FirstChild()->FirstChild();
-	lNode->ToElement()->QueryIntAttribute("cx",&lX);
-	lNode->ToElement()->QueryIntAttribute("cy",&lY);
-	lNode->ToElement()->QueryIntAttribute("r",&lR);
-	lColor = lNode->ToElement()->Attribute("fill");
 
-	gOuterCircle.setPosition(lX, lY);
-	gOuterCircle.setRadius(lR);
-	gOuterCircle.setColor(lColor);
+	gOuterCircle = ExtractCircleData(lNode->ToElement());
 
-	pWindowWidth = (GLsizei) lR * 2;
-	pWindowHeight = (GLsizei) lR * 2;
-	
+	pWindowWidth = (GLsizei) gOuterCircle.getRadius() * 2;
+	pWindowHeight = (GLsizei) gOuterCircle.getRadius() * 2;
+
 	glutInitWindowSize(pWindowWidth,pWindowHeight);
 	glutInitWindowPosition(50,50);
 	glutCreateWindow("TC2");
@@ -95,69 +91,83 @@ void CreateWindow(string pFilePath, GLsizei& pWindowWidth, GLsizei& pWindowHeigh
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho( (GLfloat) (lX - lR),(GLfloat) (lX + lR),
-						 (GLfloat) (lY - lR),(GLfloat) (lY + lR),-1.0,1.0);
+	glOrtho( 	gOuterCircle.getX() - gOuterCircle.getRadius(),gOuterCircle.getX() + gOuterCircle.getRadius(),
+						 gOuterCircle.getY() - gOuterCircle.getRadius(), gOuterCircle.getY() + gOuterCircle.getRadius(),
+						 -1.0,1.0);
 
 	lNode = lNode->NextSibling();
-	lNode->ToElement()->QueryIntAttribute("cx",&lX);
-	lNode->ToElement()->QueryIntAttribute("cy",&lY);
-	lNode->ToElement()->QueryIntAttribute("r",&lR);
-	lColor = lNode->ToElement()->Attribute("fill");
-
-	gInnerCircle.setPosition(lX, lY);
-	gInnerCircle.setRadius(lR);
-	gInnerCircle.setColor(lColor);
+	gInnerCircle = ExtractCircleData(lNode->ToElement());
 
 	lNode = lNode->NextSibling();
-	lNode->ToElement()->QueryIntAttribute("x",&lX);
-	lNode->ToElement()->QueryIntAttribute("y",&lY);
-	lNode->ToElement()->QueryIntAttribute("width",&lW);
-	lNode->ToElement()->QueryIntAttribute("height",&lH);
-	lColor = lNode->ToElement()->Attribute("fill");
-
-	gStripeRect.setPosition(lX + lW/2, lY + lH/2);
-	gStripeRect.setWidth(lW);
-	gStripeRect.setHeight(lH);
-	gStripeRect.setColor(lColor);
+	gStripeRect = ExtractRectData(lNode->ToElement());
 
 	lNode = lNode->NextSibling();
-	lNode->ToElement()->QueryIntAttribute("cx",&lX);
-	lNode->ToElement()->QueryIntAttribute("cy",&lY);
-	lNode->ToElement()->QueryIntAttribute("r",&lR);
-	lColor = lNode->ToElement()->Attribute("fill");
-
-	gEnemyCircle1.setPosition(lX, lY);
-	gEnemyCircle1.setRadius(lR);
-	gEnemyCircle1.setColor(lColor);
+	gEnemyCircle1 = ExtractCircleData(lNode->ToElement());
 
 	lNode = lNode->NextSibling();
-	lNode->ToElement()->QueryIntAttribute("cx",&lX);
-	lNode->ToElement()->QueryIntAttribute("cy",&lY);
-	lNode->ToElement()->QueryIntAttribute("r",&lR);
-	lColor = lNode->ToElement()->Attribute("fill");
-
-	gEnemyCircle2.setPosition(lX, lY);
-	gEnemyCircle2.setRadius(lR);
-	gEnemyCircle2.setColor(lColor);
+	gEnemyCircle2 = ExtractCircleData(lNode->ToElement());
 
 	lNode = lNode->NextSibling();
-	lNode->ToElement()->QueryIntAttribute("cx",&lX);
-	lNode->ToElement()->QueryIntAttribute("cy",&lY);
-	lNode->ToElement()->QueryIntAttribute("r",&lR);
-	lColor = lNode->ToElement()->Attribute("fill");
-
-	gEnemyCircle3.setPosition(lX, lY);
-	gEnemyCircle3.setRadius(lR);
-	gEnemyCircle3.setColor(lColor);
+	gEnemyCircle3 = ExtractCircleData(lNode->ToElement());
 
 	lNode = lNode->NextSibling();
-	lNode->ToElement()->QueryIntAttribute("cx",&lX);
-	lNode->ToElement()->QueryIntAttribute("cy",&lY);
-	lNode->ToElement()->QueryIntAttribute("r",&lR);
-	lColor = lNode->ToElement()->Attribute("fill");
+	gPlayerCar = ExtractCarData(lNode->ToElement());
+}
 
-	gPlayerCar.setPosition(lX, lY);
-	gPlayerCar.setRadius(lR);
-	gPlayerCar.setColor(lColor);
-	gPlayerCar.setDirection( M_PI/2 );
+// Uses global variables inside de function
+// void ExtractNodeData(TiXmlNode* pNode) {
+// 	TiXmlElement* lElement = pNode->ToElement();
+// 	string lID = lElement->Attribute("id");
+// 	if ()
+//
+// 	else if ("LargadaChegada")
+// 		gStripeRect = ExtractRectData(lElement);
+// }
+
+Circle ExtractCircleData(TiXmlElement* pElement) {
+	GLint lX, lY, lR;
+	string lColor;
+	Circle lCircle;
+	pElement->QueryIntAttribute("cx",&lX);
+	pElement->QueryIntAttribute("cy",&lY);
+	pElement->QueryIntAttribute("r",&lR);
+	lColor = pElement->Attribute("fill");
+
+	lCircle.setPosition(lX, lY);
+	lCircle.setRadius(lR);
+	lCircle.setColor(lColor);
+	return lCircle;
+}
+
+Car ExtractCarData(TiXmlElement* pElement) {
+	GLint lX, lY, lR;
+	string lColor;
+	Car lCar;
+	pElement->QueryIntAttribute("cx",&lX);
+	pElement->QueryIntAttribute("cy",&lY);
+	pElement->QueryIntAttribute("r",&lR);
+	lColor = pElement->Attribute("fill");
+
+	lCar.setPosition(lX, lY);
+	lCar.setRadius(lR);
+	lCar.setColor(lColor);
+	lCar.setDirection( M_PI/2 );
+	return lCar;
+}
+
+Rectangle ExtractRectData(TiXmlElement* pElement) {
+	GLint lX, lY, lW, lH;
+	string lColor;
+	Rectangle lRect;
+	pElement->QueryIntAttribute("x",&lX);
+	pElement->QueryIntAttribute("y",&lY);
+	pElement->QueryIntAttribute("width",&lW);
+	pElement->QueryIntAttribute("height",&lH);
+	lColor = pElement->Attribute("fill");
+
+	lRect.setPosition(lX + lW/2, lY + lH/2);
+	lRect.setWidth(lW);
+	lRect.setHeight(lH);
+	lRect.setColor(lColor);
+	return lRect;
 }
